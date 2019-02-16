117552a7360849180629bfdd7d5dcb40dcab57af
halosghost <samuel.stuewe@gmail.com>
2019-02-16 09:29:10 -0600

    add loader.h (on the path to make things cleaner)

--- src/loader.c
+++ src/loader.c
@@ -1,15 +1,4 @@
-#include <dlfcn.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
-#include <stdbool.h>
-#include <errno.h>
-#include <libgen.h>
-#include <unistd.h>
-#include <sys/types.h>
-
-#define MAXMODS 100
+#include "loader.h"
 
 signed
 main (signed argc, char * argv []) {
---
+++ src/loader.h
@@ -0,0 +1,12 @@
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <libgen.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define MAXMODS 100
